7d2fb7afe9680d8c9c41f58f0f153ff113a40909
O'Ryan <oryanalexander_1@hotmail.com>
2020-05-25 21:17:57 -0400

    doc: changes to documents

--- server.js
+++ server.js
@@ -1,45 +1,53 @@
-// Setup empty JS object to act as endpoint for all routes
-const projectData = {};
-
-// Express to run server and routes
+// Imports
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Start up an instance of app
 const app = express();
 
 /* Dependencies */
 /* Middleware*/
-const bodyParser = require("body-parser");
-
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static("website"));
 
-// Spin up the server
-const port = 8000;
-
-const listening = () => {
-  console.log("Server Running");
-  console.log("Server Listening on port:" + port);
-};
-
-const server = app.listen(port, listening);
+// set up port
+const port = process.env.PORT || 8000;
 
 // Callback to debug
+const listening = () => {
+  console.log("Server Running");
+  console.log(`Server is on port:${port}`);
+};
+
+// Spin up the server
+const server = app.listen(port, listening);
+
+// Setup empty JS object to act as endpoint for all routes
+let projectData = [];
 
 // Callback function to complete GET '/all'
 const all = (req, res) => {
-  res.send("hello im working");
+  res.send(projectData);
 };
 
 // Initialize all route with a callback function
 app.get("/all", all);
 
 // Post Route
+app.post("/weather", (req, res) => {
+  newEntry = {
+    temp: req.body.main.temp,
+    facts: req.body.main.feels_like,
+    hot: req.body
+  };
+  projectData.push(newEntry);
+  console.log(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,17 +1,56 @@
 /* Global Variables */
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const city = document.getElementById("city");
+
+// Personal API Key for OpenWeatherMap API
+const apiKey = "&APPID=e3d02ea1a9fad9177d217738809587cd";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
-// Personal API Key for OpenWeatherMap API
-
 // Event listener to add function to existing HTML DOM element
+document.getElementById("generate").addEventListener("click", preformAction);
 
 /* Function called by event listener */
+function preformAction(event) {
+  const newCity = city.value;
+  const weather = getWeather(baseUrl, newCity, apiKey).then(function(data) {
+    postData("/weather", data);
+  });
+}
 
 /* Function to GET Web API Data*/
+const getWeather = async (baseUrl, city, apiKey) => {
+  const response = await fetch(baseUrl + city + apiKey);
+  try {
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log("err", err);
+  }
+};
 
 /* Function to POST data */
+const postData = async (url = "", data = {}) => {
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  try {
+    const newData = response.json();
+    console.log(newData);
+    return newData;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
 
 /* Function to GET Project Data */
